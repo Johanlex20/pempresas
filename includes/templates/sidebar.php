29a407b8df9e9882db32f70952a45c3ca3954266
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2023-05-04 13:21:21 -0500

    Modificacion de ruta admin cerrar seccion
    se modifico la ruta de admin. perfil ya que al momento de cerrar sesión daba error ya que no reconocia la ruta

--- includes/templates/sidebar.php
+++ includes/templates/sidebar.php
@@ -3,7 +3,7 @@
 
     <nav class="sidebar-nav">
     <a href="/index.php">HOME</a>
-        <a href="/admin/index.php">PERFIL</a>
+        <a href="/admin">PERFIL</a>
         <a href="/admin/propiedades/crear.php">CREAR USUARIO</a>
         <a href="/admin/propiedades/consultar.php">CONSULTAR USUARIO</a>
     </nav>

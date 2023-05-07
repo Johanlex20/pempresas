a9f51df3ba4b14e28a76aaa616005b6e133d8e79
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2023-05-06 21:06:59 -0500

    POO 367 SANITIZACION DE DATOS
    se establece en el archivo aprendiz la columna para recorrer un array que establecera y funcionara en atributos que recorrera y sanitizara cualquier intento de inyecion

--- classes/Aprendiz.php
+++ classes/Aprendiz.php
@@ -6,6 +6,8 @@ class aprendiz {
 
     //BASE DE DATOS
     protected static $db;
+    //IDENTIFICO QUE FORMA VA TENER LA COLUMNA
+    protected static $columnasDB = ['id', 'nombre', 'tipoId', 'identificacion', 'programa', 'email', 'password', 'telefono', 'creacionaprendiz'];
 
     public $id;
     public $nombre;
@@ -16,6 +18,11 @@ class aprendiz {
     public $password;
     public $telefono;
     public $creacionaprendiz;
+
+    //DEFINIR LA CONEXION A LA BD
+    public static function setDB($database){
+        self::$db = $database;
+    }
 
     public function __construct ($args = []){
         //??''= En caso de que no este lleno se agrega un strim vacío
@@ -31,16 +38,36 @@ class aprendiz {
     }
     
     public function guardar(){
-        
 
+    // SANITIZAR DATOS
+    $atributos = $this->sanitizarAtributos();
+    debuguear($atributos);
+        
+    //INSERTAR DATOS EN LA BASE DE DATOS
         $query = " INSERT INTO aprendiz (nombre, tipoId, identificacion, programa, email, password, telefono, creacionaprendiz) VALUES ('$this->nombre', '$this->tipoId', '$this->identificacion', '$this->programa', '$this->email', '$this->password', '$this->telefono' , '$this->creacionaprendiz')";
 
         $resultado = self::$db->query($query);
         debuguear($resultado);
     }
 
-    //DEFINIR LA CONEXION A LA BD
-    public static function setDB($database){
-        self::$db = $database;
+    //IDENTIFICAR Y UNIR LOS ATRIBUTOS DE LA DB
+    public function atributos(){
+        $atributos = [];
+        foreach(self::$columnasDB as $columna){
+            if($columna === 'id') continue;
+            $atributos[$columna] = $this->$columna;
+        }
+        return $atributos;
     }
+
+    public function sanitizarAtributos(){
+        $atributos = $this->atributos();
+        $sanitizado = [];  
+        foreach($atributos as $key => $value ){
+            $sanitizado[$key] = self::$db->escape_string($value);
+        }
+        return $sanitizado;
+    }
+
+
 }

0922e8318bdff834989cc2a882d9a0491439c172
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2023-05-12 19:35:56 -0500

    Trabajando el el responsive ofertas
    trabajando en el responsive de ofertas

--- index.php
+++ index.php
@@ -47,20 +47,20 @@
                         <p>Solicita Programadores con conocimientos en HTML, CSS, python</p>
                         <p class="precio">$3.000.000</p>  
 
-                        <ul class="iconos-caja">
-                            <li>
+                        <div class="iconos-caja">
+                            <div>
                                 <img loading="lazy" src="/build/img/icono-empresa.webp" alt="icono-persona"> 
                                 <p>postulacion: 32</p>    
-                            </li>
-                            <li>
+                            </div>
+                            <div>
                                 <img loading="lazy" src="/build/img/icono-email.webp" alt="icono-email">
                                 <p>google@gmail.com</p>
-                            </li>
-                            <li>
+                            </div>
+                            <div>
                                 <img loading="lazy" src="/build/img/icono-telefono.webp" alt="icono-telefono">
                                 <p>300100100</p>
-                            </li>      
-                        </ul>
+                            </div>      
+                        </div>
                     </div>
                     <div class="caja_ofe">
                         <picture>
@@ -72,20 +72,45 @@
                         <p>Solicita Programadores con conocimientos en HTML, CSS, python</p>
                         <p class="precio">$3.000.000</p>  
 
-                        <ul class="iconos-caja">
-                            <li>
+                        <div class="iconos-caja">
+                            <div>
                                 <img loading="lazy" src="/build/img/icono-empresa.webp" alt="icono-persona"> 
                                 <p>postulacion: 32</p>    
-                            </li>
-                            <li>
+                            </div>
+                            <div>
                                 <img loading="lazy" src="/build/img/icono-email.webp" alt="icono-email">
                                 <p>google@gmail.com</p>
-                            </li>
-                            <li>
+                            </div>
+                            <div>
                                 <img loading="lazy" src="/build/img/icono-telefono.webp" alt="icono-telefono">
                                 <p>300100100</p>
-                            </li>      
-                        </ul>
+                            </div>      
+                        </div>
+                    </div>
+                    <div class="caja_ofe">
+                        <picture>
+                            <source srcset="build/img/anuncio1.webp" type="image/webp">
+                            <source srcset="build/img/anuncio1.jpg" type="image/jpeg">
+                            <img loading="lazy" src="build/img/anuncio1.jpg" alt="anuncio">
+                        </picture>
+                        <h3>Programadores de Software</h3>
+                        <p>Solicita Programadores con conocimientos en HTML, CSS, python</p>
+                        <p class="precio">$3.000.000</p>  
+
+                        <div class="iconos-caja">
+                            <div>
+                                <img loading="lazy" src="/build/img/icono-empresa.webp" alt="icono-persona"> 
+                                <p>postulacion: 32</p>    
+                            </div>
+                            <div>
+                                <img loading="lazy" src="/build/img/icono-email.webp" alt="icono-email">
+                                <p>google@gmail.com</p>
+                            </div>
+                            <div>
+                                <img loading="lazy" src="/build/img/icono-telefono.webp" alt="icono-telefono">
+                                <p>300100100</p>
+                            </div>      
+                        </div>
                     </div>
             </div>            
     </div>
